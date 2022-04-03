05945786ee5bd4dcdf6b8f1ca85240ebf33c3aee
eelyort <troylee3006@gmail.com>
2022-04-03 17:37:31 -0500

    a

--- build/examples_rclcpp_minimal_client/colcon_command_prefix_test.bat
+++
@@ -1,3 +0,0 @@
-:: generated from colcon_core/shell/template/command_prefix.bat.em
-@echo off
-call "D:\Troy\Extra Curriculars\Turtle\MazeBot\dev\ros2-mazebot-project\install\share\examples_rclcpp_minimal_client\package.bat"
