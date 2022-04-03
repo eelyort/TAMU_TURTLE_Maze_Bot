6c33179c00db36056be2698f815d92c6fa41ec41
eelyort <troylee3006@gmail.com>
2022-04-03 17:37:50 -0500

    a

--- build/examples_rclcpp_minimal_subscriber/ament_cmake_package_templates/templates.cmake
+++
@@ -1,8 +0,0 @@
-set(ament_cmake_package_templates_ENVIRONMENT_HOOK_LIBRARY_PATH )
-set(ament_cmake_package_templates_ENVIRONMENT_HOOK_PYTHONPATH "D:/Troy/Extra Curriculars/Turtle/MazeBot/dev/ros2-windows/Lib/site-packages/ament_package/template/environment_hook/pythonpath.bat.in")
-set(ament_cmake_package_templates_PACKAGE_LEVEL "")
-list(APPEND ament_cmake_package_templates_PACKAGE_LEVEL "D:/Troy/Extra Curriculars/Turtle/MazeBot/dev/ros2-windows/Lib/site-packages/ament_package/template/package_level/local_setup.bat.in")
-set(ament_cmake_package_templates_PREFIX_LEVEL "")
-list(APPEND ament_cmake_package_templates_PREFIX_LEVEL "D:/Troy/Extra Curriculars/Turtle/MazeBot/dev/ros2-windows/Lib/site-packages/ament_package/template/prefix_level/local_setup.bat.in")
-list(APPEND ament_cmake_package_templates_PREFIX_LEVEL "D:/Troy/Extra Curriculars/Turtle/MazeBot/dev/ros2-windows/Lib/site-packages/ament_package/template/prefix_level/setup.bat.in")
-list(APPEND ament_cmake_package_templates_PREFIX_LEVEL "D:/Troy/Extra Curriculars/Turtle/MazeBot/dev/ros2-windows/Lib/site-packages/ament_package/template/prefix_level/_local_setup_util.py")
