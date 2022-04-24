8e4a2068cbec8b118799edf53e27bf72689ea7e5
eelyort <troylee3006@gmail.com>
2022-04-24 17:39:58 -0500

    hi

--- install/setup.ps1
+++ install/setup.ps1
@@ -24,6 +24,7 @@ function _colcon_prefix_chain_powershell_source_script {
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "D:\Troy\Extra Curriculars\Turtle\MazeBot\dev\ros2-foxy-20220208-windows-debug-amd64\ros2-windows\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "D:\Troy\Extra Curriculars\Turtle\MazeBot\dev\ros2-foxy-20220208-windows-debug-amd64\ros2-windows\\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "D:\Troy\Extra Curriculars\Turtle\MazeBot\dev\ros2-mazebot-project\install\\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
